74672843d3be5ab147f767539e2da6bd84587d16
Matheus Alves <maalves@redhat.com>
2026-09-23 18:08:33 -0300

    Fix init

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 
 # Initialize the Flask application
-app = Flask(__name__)
+application = Flask(__name__)
 
 # Define the route for the homepage
-@app.route("/")
+@application.route("/")  # <--- Changed from @app to @application
 def hello_world():
     return "Hello, World!"
 
-# Run the local development server
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(host="0.0.0.0", port=8080)
